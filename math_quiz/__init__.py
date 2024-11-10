3dda34f47b371f98978ba47540ec413e6bf72fc2
Atif1038 <atifalianwar1038@gmail.com>
2024-11-10 20:42:01 +0500

    adding setup.py and init.py

---
+++ math_quiz/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 17:26:38 2024
+
+@author: Atif Ali
+"""
+
+from .math_quiz import generate_random_number, generate_random_operator, calculate_answer
+
---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Nov 10 17:08:27 2024
+
+@author: Atif Ali
+"""
+
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',                 # The name of your package
+    version='0.1',                    # Version of the package
+    packages=find_packages(),         # Automatically find the `math_quiz` package
+    install_requires=[],              
+    description='A simple math quiz game with functions for question generation and answer calculation',
+    author='Atif Ali Anwar',               
+    author_email='atifalianwar1038@gmail.com',  
+    url='https://github.com/ATIFALI1038/dsss_homework_2.git',  # Link to your GitHub repository
+)
